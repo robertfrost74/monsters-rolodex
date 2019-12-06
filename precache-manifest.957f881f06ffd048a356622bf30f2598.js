699428c1a10a407e89fa29817e472517f05dccba
yardsman <robfro@me.com>
2019-12-06 19:11:44 +0100

    Updates

---
+++ precache-manifest.957f881f06ffd048a356622bf30f2598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a55e5f0cd400028b855746026461e6b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d7405a174483f187e20",
+    "url": "/monsters-rolodex/static/css/main.4bbee0f7.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d7405a174483f187e20",
+    "url": "/monsters-rolodex/static/js/main.24572f34.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
